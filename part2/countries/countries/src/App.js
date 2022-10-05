64895daaa6dc7bbcd0a1d6102adf4c6696756a7b
tarantino19 <tolentinored19@gmail.com>
2022-10-05 16:35:15 +0800

    skipped 2.13-weather next

--- part2/countries/countries/src/App.js
+++ part2/countries/countries/src/App.js
@@ -6,6 +6,8 @@ import axios from 'axios'
 function App () {
   const [countries, setCountries] = useState ([])
   const [search, setSearch] = useState ("")
+  // const [showCountry, setShowCountry] = useState ({})
+  // const [weather, getWeather] = useState ({})
 
   useEffect(() => {
     axios
@@ -20,20 +22,27 @@ function App () {
       )
   }, [])
 
-  const handleChange = event => setSearch (event.target.value)
+const handleChange = event => setSearch (event.target.value)
 
 const filteredCountries = countries.filter (country => country.name.toLowerCase().includes (search.toLowerCase ()))
 
-console.log(countries);
+//implicitly returns another function
+// const handleSetShow = name  => () => {
+//   setShowCountry (
+//     filteredCountries.filter (country => country.name.map (country.name)))
+// }
+
+
 
   return (
     <div>
         <p>find countries <input value={search} onChange={handleChange} /></p>
         {filteredCountries.length > 10 && (
           <div>Go specify another filter</div>)}
-        {filteredCountries.length <= 10 && filteredCountries.length > 1 && filteredCountries.map (country => <div key={country.name.common}>{country.name} {country.name.common}</div>)}
+        {filteredCountries.length <= 10 && filteredCountries.length > 1 && filteredCountries.map (country => <div key={country.name.common}>{country.name} <button>show</button></div>)
+      }
         {filteredCountries.length === 1 &&  (
-          <div>
+          <>
               <h1>{filteredCountries[0].name}</h1>
               <p>capital: {filteredCountries[0].capital}</p>
               <p>area: {filteredCountries[0].area}</p>
@@ -43,65 +52,20 @@ console.log(countries);
                   <li key={country}>{country}</li>
                 )}
               </ul>
-          <div><img src={filteredCountries[0].flags.png} alt=""/></div>
-          </div>
-        )
+          <div><img src={filteredCountries[0].flags.png} alt={`${filteredCountries[0].name} flag`}/></div>
+              <h3>Weather in {filteredCountries[0].capital}</h3>
+              <div>temperature</div>
+          </>
+        ) 
       }
 
 
 
-
-        
     </div>
   )
 
 }
 
-
-// function App () {
-//   const [countries, setCountries] = useState ([])
-//   const [newCountry, setCountrySearch] = useState ("")
-  
-//   useEffect(() => {
-//     axios
-//       .get('https://restcountries.com/v3.1/all')
-//       .then(response => {
-//         setCountries(response.data)
-//       })
-//   }, [])
-
-//   function searchCountry (event) {
-//         event.preventDefault ()
-//   }
-
-//   function handleCountrySearch (event) {
-//     setCountrySearch(event.target.value)
-//   }
-
-  
-// return (
-//     <div>
-
-//     <form onChange={searchCountry}>
-
-//     <div>
-//     find countries: <input  
-//     type="search"
-//     value={newCountry} 
-//     onChange={handleCountrySearch}
-//     > </input>
-//   </div>
-
-//       <>{countries.filter (country => country.name.toLowerCase ().includes (newCountry)).map (country=> (
-//         <div key={country.name}>{country.name}</div>
-//       ))
-//   }</>
-
-// </form>
-
-//     </div>
-//   )
-// }
 
 export default App;
 
