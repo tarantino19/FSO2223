5680d518248faa64e4ef7f6843029855a50996ef
tarantino19 <tolentinored19@gmail.com>
2022-10-14 17:30:09 +0800

    refactor

--- part3-forstudying/backend/index.js
+++ part3-forstudying/backend/index.js
@@ -2,9 +2,6 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-app.use(cors())
-app.use(express.static('build'))
-
 let notes = [
   {
     id: 1,
@@ -37,6 +34,10 @@ const requestLogger = (request, response, next) => {
 app.use(express.json())
 
 app.use(requestLogger)
+
+app.use(cors())
+
+app.use(express.static('build'))
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
