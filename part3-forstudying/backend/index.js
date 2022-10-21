4aab83c563b0d193ab8080da17ac9504d6aa3c0f
tarantino19 <tolentinored19@gmail.com>
2022-10-21 15:12:15 +0800

    es lint

--- part3-forstudying/backend/index.js
+++ part3-forstudying/backend/index.js
@@ -63,10 +63,10 @@ app.put('/api/notes/:id', (request, response, next) => {
   const { content, important } = request.body
 
   Note.findByIdAndUpdate(
-    request.params.id, 
+    request.params.id,
     { content, important },
     { new: true, runValidators: true, context: 'query' }
-  ) 
+  )
     .then(updatedNote => {
       response.json(updatedNote)
     })
